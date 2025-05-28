baa6415f07f27e2c1eabc8a729c50d0c60f82b8f
Student <millenkahsaygirmay@gmail.com>
2025-05-28 16:01:22 -0700

    Initial commit

--- src/DrinkSize.java
+++ src/DrinkSize.java
@@ -1,18 +1,5 @@
-public class DrinkSize implements MenuItem{
-    private String size;
-    private double price;
-    private String flavor;
-
-    public DrinkSize(String size, double price, String flavor){
-        this.size = size;
-        this.price = price;
-        this.flavor = flavor;
-    }
-
-    public String getName(){
-        return size + " Drink - " + flavor;
-    }
-    public double getPrice(){
-        return price;
-    }
+public class DrinkSize {
+    static String Small = "small";
+    static String Medium = "medium";
+    static String Large = "large";
 }
