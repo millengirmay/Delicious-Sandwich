798f394969e8984176c653e12ef3b4394a99ecfb
Student <millenkahsaygirmay@gmail.com>
2025-05-28 15:24:13 -0700

    Initial commit

--- src/SignatureSandwich.java
+++ src/SignatureSandwich.java
@@ -1,2 +1,20 @@
-public class SignatureSandwich {
+import java.util.ArrayList;
+import java.util.List;
+
+public class SignatureSandwich extends Sandwich{
+    public SignatureSandwich(String name){
+        super("8\"",
+                new Bread("white", 7.00),
+                createToppings(name),
+                true);
+    }
+
+    private static List<Topping> createToppings(String name){
+        List<Topping> toppings = new ArrayList<>();
+        if(name.equalsIgnoreCase("blt")){
+            toppings.add(new Topping("bacon", 2.50, false));
+            toppings.add(new Topping("bacon", 2.50, false));
+            toppings.add(new Topping("bacon", 2.50, false))
+        }
+    }
 }
