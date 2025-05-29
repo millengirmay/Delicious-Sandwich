b5b7c2d65a592601683072372e0b11588c8aa115
Student <millenkahsaygirmay@gmail.com>
2025-05-28 17:06:36 -0700

    Initial commit

--- src/Sandwich.java
+++ src/Sandwich.java
@@ -1,33 +1,48 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-public class Sandwich implements MenuItem{
-    private String size;
-    private Bread bread;
-    private List<Topping> toppings;
-    private boolean toasted;
+public class Sandwich {
+    String size;
+    String bread;
+    boolean toasted;
+    List<Topping> toppings = new ArrayList<>();
 
-    public Sandwich(String size, Bread bread, List<Topping> toppings, boolean toasted){
+    public Sandwich(String size, String bread, boolean toasted) {
         this.size = size;
         this.bread = bread;
-        this.toppings = toppings;
         this.toasted = toasted;
     }
 
-    public String getName(){
-        return size + " Sandwich on " + bread.getName() + (toasted ? "(Toasted)": "");
+    public void addTopping(Topping topping) {
+        toppings.add(topping);
     }
 
-    public double getPrice(){
-        double toppingTotal = toppings.stream().mapToDouble(MenuItem::getPrice).sum();
-        return bread.getPrice() + toppingTotal;
+    public double getBasePrice() {
+        return switch (size) {
+            case Size.Four -> 5.50;
+            case Size.Eight -> 7.00;
+            case Size.Twelve -> 8.50;
+            default -> 0.0;
+        };
     }
 
-    public String getDetails(){
+    public double calculatePrice() {
+        Map<String, Long> countMap = toppings.stream()
+                .collect(Collectors.groupingBy(Topping::getName, Collectors.counting()));
+
+        return getBasePrice() + toppings.stream()
+                .mapToDouble(t -> t.getPrice(size, countMap.get(t.getName()) > 1))
+                .sum();
+    }
+
+    public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append(" ").append(getName()).append("\n");
-        for(Topping t: toppings){
-            sb.append("  -").append(t.getName()).append("($").append(String.format("%.2f", t.getPrice()));
-        }
+        sb.append(size).append(" ").append(bread).append(" sandwich ");
+        if (toasted) sb.append("Toasted ");
+        toppings.forEach(t -> sb.append(" - ").append(t.getName()).append(" "));
+                        sb.append(String.format("Subtotal: $%.2f ", calculatePrice()));
         return sb.toString();
     }
 }
---
+++ src/Sauce.java
@@ -0,0 +1,8 @@
+public class Sauce extends Topping{
+    public Sauce(String name){
+        super(name);
+    }
+    public double getPrice(String size, boolean isExtra){
+        return 0.0;
+    }
+}
